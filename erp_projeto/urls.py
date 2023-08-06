a0312514d540b141518701452de49159181d88b8
Geovanne Murata <geovanne1997@hotmail.com>
2023-08-06 03:03:19 -0300

    Removi pag alterar produtos

--- erp_projeto/urls.py
+++ erp_projeto/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
     # path('inicio/', views.home, name='home'),
     path('produtos/', views.produtos_filtro, name='listagem_produtos'),
     path('cadastrar/', views.cadastrar, name='cadastrar'),
-    path('alterar_produto/<int:sku>/<int:new_qtde>/', views.alterar, name='alterar_produto'),
     path('deletar_produto/<int:sku>/', views.deletar, name='deletar_produto'),
     path('login/', views.login, name='login'),
     path('admin/', admin.site.urls),
