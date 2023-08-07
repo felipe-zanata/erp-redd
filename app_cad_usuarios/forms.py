4fdb45659f95fa5d1f6e442179f17d84125d2514
Geovanne Murata <geovanne1997@hotmail.com>
2023-08-07 00:35:41 -0300

    Importar Excel

--- app_cad_usuarios/forms.py
+++ app_cad_usuarios/forms.py
@@ -9,4 +9,4 @@ class UsuarioForm(ModelForm):
         fields = '__all__'
 
 class ExcelImportForm(forms.Form):
-    excel_file = forms.FileField(label='')
+    excel_file = forms.FileField(label='', widget=forms.FileInput(attrs={'class': 'custom-file-input', 'id': 'fileInput'}))
