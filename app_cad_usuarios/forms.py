62bb8d9b78bc84d9c524a47c4fc085e564cd61d9
LuizEduardo-service <luizeduardo.service@outlook.com>
2023-08-15 18:08:47 -0300

    ajustando importação de excel

--- app_cad_usuarios/forms.py
+++ app_cad_usuarios/forms.py
@@ -1,20 +1,6 @@
-from django.forms import ModelForm
-from .models import Usuario
-# from .models import FiltroMovientacao
 from django import forms
 
-# ProdutoForm
-class UsuarioForm(ModelForm):
-    class Meta:
-        model = Usuario # Produto
-        fields = '__all__'
 
 class ExcelImportForm(forms.Form):
     excel_file = forms.FileField(label='', widget=forms.FileInput(attrs={'class': 'custom-file-input', 'id': 'fileInput'}))
 
-
-# ProdutoForm
-# class ItemFilterMovimentacao(forms.Form):
-    # txt_cod_produto = forms.CharField(max_length=50, required=False)
-    # txt_nome_produto = forms.CharField(max_length=50, required=False)
-    # txt_qtde_produto = forms.IntegerField(max_length=9999999999, required=False)
--- app_cad_usuarios/models.py
+++ app_cad_usuarios/models.py
@@ -1,13 +1,2 @@
 from django.db import models
 # Produto
-class Usuario(models.Model):
-    id_usuario = models.AutoField(primary_key=True)
-    codigo_produto = models.TextField()
-    nome_produto = models.TextField()
-    qtidade_produto = models.IntegerField()
-    obs_produto = models.TextField()
-
-# class FiltroMovientacao(models.Model):
-#     txt_cod_produto = models.CharField(max_length=50)
-#     txt_nome_produto = models.CharField(max_length=50)
-#     txt_qtde_produto = models.IntegerField(max_length=50)
